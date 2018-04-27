78e5917fc3d8a143e61ac485a7adcc05ffe4d0aa
jemtca <30645648+jemtca@users.noreply.github.com>
2018-04-27 13:10:23 -0700

    Objective-C Development

---
+++ Assignment 3/ContactList.h
@@ -0,0 +1,17 @@
+//Jesús Mata Camacho
+
+#import <Foundation/Foundation.h>
+#import "Contact.h"
+#import "InputCollector.h"
+
+@interface ContactList : NSObject //: == extends
+
+#pragma mark - Public Properties
+@property NSMutableArray <Contact *> *contactList;
+
+#pragma mark - Public Methods
+- (instancetype) init;
+- (void) addContact:(Contact *) newContact;
+- (void) printContactList;
+
+@end
---
+++ Assignment 3/ContactList.m
@@ -0,0 +1,43 @@
+//Jesús Mata Camacho
+
+#import "ContactList.h"
+
+@implementation ContactList
+
+- (instancetype)init{
+    
+    self = [super init]; //self == this
+    
+    if(self){
+        
+        self.contactList = [NSMutableArray new]; //[NSMutableArray new] == [[NSMutableArray alloc] init]
+        
+    }
+    
+    return self;
+    
+}
+
+- (void) addContact:(Contact *)newContact{
+    
+    [[self contactList] addObject:newContact]; //ask Rodrigo: _contactList
+    
+}
+
+- (void) printContactList{
+    
+    int i = 1;
+    
+    [InputCollector printToPrompt:@""];
+    
+    for(Contact *temp in [self contactList]){
+        
+        [InputCollector printToPrompt:[NSString stringWithFormat:@"[Contact %d]", i]];
+        [InputCollector printToPrompt:[NSString stringWithFormat:@"%@", [temp getContactDetails]]];
+        i++;
+        
+    }
+    
+}
+
+@end
